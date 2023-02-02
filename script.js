982b706cf226c3e5d11ad7f05055ddfb90c973ab
Ethan <ethan.djanogly@gmail.com>
2023-02-02 11:22:12 +0200

    added grid to court and tweaked styles

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-let tafges = 33
+function createBasketBallDivs () {
+    let basketballDiv = document.getElementById("basketballCourt")
+    console.log("here")
+    for (let i=0; i < 800; i++){
+        console.log(i)
+        let createdDiv = document.createElement("div")
+        createdDiv.id = `div${i}`
+        createdDiv.style.border = "solid 0.25px green"
+        basketballDiv.appendChild(createdDiv)
+    }
+}
+
+createBasketBallDivs()
