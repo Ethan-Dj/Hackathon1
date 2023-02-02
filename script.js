0dafa8c1278ac03f4b5dd981d418bdf57ef5fb41
Ethan <ethan.djanogly@gmail.com>
2023-02-02 12:09:25 +0200

    Created Basketball Divs without event listeners

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 function createBasketBallDivs () {
     let basketballDiv = document.getElementById("basketballCourt")
     console.log("here")
-    for (let i=0; i < 800; i++){
+    for (let i=0; i < 220; i++){
         console.log(i)
         let createdDiv = document.createElement("div")
         createdDiv.id = `div${i}`
         createdDiv.style.border = "solid 0.25px green"
+
+
+        createdDiv.addEventListener("mousedown",mouseDown)
+        createdDiv.addEventListener("mousedown",mouseUp)
+        createdDiv.addEventListener("key",amount)
+        createdDiv.addEventListener("mouseover",location)
+
         basketballDiv.appendChild(createdDiv)
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 createBasketBallDivs()
