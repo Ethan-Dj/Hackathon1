c4c47eb8bf265a8274155aaaecfd54ecdcb8cf57
Ethan <ethan.djanogly@gmail.com>
2023-02-02 12:52:53 +0200

    added all the event listeners, without functionality

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 function createBasketBallDivs () {
     let basketballDiv = document.getElementById("basketballCourt")
-    console.log("here")
     for (let i=0; i < 220; i++){
-        console.log(i)
         let createdDiv = document.createElement("div")
         createdDiv.id = `div${i}`
         createdDiv.style.border = "solid 0.25px green"
@@ -10,15 +8,38 @@ function createBasketBallDivs () {
 
         createdDiv.addEventListener("mousedown",mouseDown)
         createdDiv.addEventListener("mousedown",mouseUp)
-        createdDiv.addEventListener("key",amount)
-        createdDiv.addEventListener("mouseover",location)
+        document.addEventListener("keydown",amount)
+        createdDiv.addEventListener("mouseover",locations)
 
         basketballDiv.appendChild(createdDiv)
     }
 }
 
+let isMouseDown = false;
+let keyPressed = "";
+let currentLocation = ""
 
 
+function mouseDown(evt){
+    evt.preventDefault()
+    isMouseDown = true
+}
+
+function mouseUp(evt){
+    evt.preventDefault()
+    isMouseDown = false
+    console.log(isMouseDown)
+}
+
+function amount(evt){
+    evt.preventDefault()
+    keyPressed = evt.key
+}
+
+function locations(evt){
+    evt.preventDefault()
+    currentLocation = evt.srcElement.attributes.id.nodeValue
+}
 
 
 
